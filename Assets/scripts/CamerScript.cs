3f70c04b136ee84005d9c5fce292c806858776b3
jamiejamiejamiejamie <game.jamie@hotmail.com>
2015-10-14 13:07:41 -0400

    added navigation
    still need tweeking, but it can move now

---
+++ Assets/scripts/CamerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerScript : MonoBehaviour {
+	public GameObject shipcamera;
+	public GameObject ship;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		shipcamera = GameObject.Find ("Main Camera");
+		ship = GameObject.Find ("Ship");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		shipcamera.GetComponent<Transform> ().position = ship.GetComponent<Transform> ().position + new Vector3 (0, 0, -10);
+	}
+}
--- Assets/scripts/Navigator.cs
+++ Assets/scripts/Navigator.cs
@@ -2,14 +2,52 @@
 using System.Collections;
 
 public class Navigator : MonoBehaviour {
-
+	public GameObject ship;
+	public float thrust = 400;
+	private Rigidbody2D shiprigidbody;
+	//private float acceleratetime = 1;
+	//private int gyrocount;
+	
 	// Use this for initialization
 	void Start () {
-	
+		ship = GameObject.Find("Ship");
+		shiprigidbody = ship.GetComponent<Rigidbody2D> ();
+
+		//gyrocount = ship.GetComponent <Ship> ().gyroCount;
+		//Vector3 up = gameObject.transform.up;
 	}
+	
+	
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		float moverotation = Input.GetAxis ("Horizontal");
+		float moveforward = Input.GetAxis ("Vertical");
+		print (moveforward);
+
+		if (moverotation != 0) {
+			ship.transform.localEulerAngles += new Vector3 (0, 0, moverotation * -5f);
+		}
+		
+		if (moveforward != 0) {
+			shiprigidbody.AddForce(ship.transform.up * thrust * moveforward);
+
+		}
+
+		
+		/*if (Input.GetKeyDown (KeyCode.W)) {
+			shiprigidbody.AddForce ( new Vector2 (0 ,thrust*acceleratetime));
+			acceleratetime += 1;
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			shiprigidbody.AddForce (new Vector2 (0, -thrust));
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			shiprigidbody.AddForce (new Vector2 (-thrust, 0));
+		}
+		if (Input.GetKeyDown (KeyCode.D)){
+			shiprigidbody.AddForce (new Vector2 (thrust, 0));
+		}*/
 	}
 }
